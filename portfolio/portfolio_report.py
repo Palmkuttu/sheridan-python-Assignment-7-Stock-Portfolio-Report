4ada906ac00e10b36da70bbf5f17145520c01068
Dennis Zacharia <dennyzach15@gmail.com>
2026-04-06 17:14:18 -0400

    Update portfolio_report.py

--- portfolio/portfolio_report.py
+++ portfolio/portfolio_report.py
@@ -1,40 +1,34 @@
-from collections import OrderedDict
 import csv
+from collections import OrderedDict
+
 
 def read_portfolio(filename):
-    data = []
-    with open(filename, "r") as file:
+    result = []
+
+    with open(filename, 'r') as file:
         reader = csv.DictReader(file)
+
         for row in reader:
-            item = OrderedDict()
-            item["symbol"] = row["symbol"]
-            item["units"] = row["units"]
-            item["cost"] = row["cost"]
-            data.append(item)
-    return data
+            ordered = OrderedDict()
+            ordered['symbol'] = row['symbol']
+            ordered['units'] = row['units']
+            ordered['cost'] = row['cost']
+            result.append(ordered)
+
+    return result
+
 
 def save_portfolio(data, filename):
-    with open(filename, "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["symbol", "units", "cost"])
-        writer.writeheader()
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
 
+        # header
+        writer.writerow(['symbol', 'units', 'cost'])
+
+        # rows
         for item in data:
-            writer.writerow(item)
-
-
-def get_args(args=None):
-    parser = argparse.ArgumentParser(description="Portfolio Report")
-    parser.add_argument("filename")
-    return parser.parse_args(args)
-
-
-def main():
-    args = get_args()
-    data = read_portfolio(args.filename)
-
-    for item in data:
-        print(item)
-
-
-if __name__ == "__main__":
-    main()
+            writer.writerow([
+                item['symbol'],
+                item['units'],
+                item['cost']
+            ])
