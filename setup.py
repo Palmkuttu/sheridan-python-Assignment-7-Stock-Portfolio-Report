eb23c22fe61b843c9326378af7da2d42f50ca382
Dennis Zacharia <dennyzach15@gmail.com>
2026-04-16 15:21:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="portfolio-report",
     version="1.0",
-    packages=find_packages(),
+    py_modules=["portfolio_report"],   
     install_requires=[
         "requests"
     ],
     entry_points={
         "console_scripts": [
-            "portfolio_report=portfolio.portfolio_report:main"
+            "portfolio_report=portfolio_report:main"   
         ]
     },
     author="Your Name",
